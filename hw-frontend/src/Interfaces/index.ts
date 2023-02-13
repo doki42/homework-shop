37d75e3bbe4fb8a505e5e9b29b1c6741442a3585
doki42 <andras.szogi@gmail.com>
2023-02-13 09:58:41 +0100

    Add interfaces

---
+++ hw-frontend/src/Interfaces/index.ts
@@ -0,0 +1,4 @@
+export type { ICredentials } from './credentials.interface';
+export type { IProduct } from './product.interface';
+export type { INewProduct } from './new-product.interface';
+export type { IUser } from './user.interface';
---
+++ hw-frontend/src/Interfaces/new-product.interface.ts
@@ -0,0 +1,6 @@
+export interface INewProduct {
+    name: string;
+    price: number;
+    picture: string;
+    active: boolean;
+}
