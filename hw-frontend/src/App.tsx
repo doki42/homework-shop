e0a60980ef61f3edcf5c75bc68a88d36412162b4
doki42 <andras.szogi@gmail.com>
2023-02-13 10:00:25 +0100

    Create page structure

--- hw-frontend/src/App.tsx
+++ hw-frontend/src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment, useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './Components/NavBar';
+import { LogIn, Home, AddProduct } from './Pages';
+import { Container } from 'react-bootstrap';
+import { IUser } from './Interfaces';
 
 function App() {
+  const [currentUser, setCurrentUSer] = useState<IUser|null>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Routes>
+          <Route path='/' element={<NavBar currentUser={currentUser} onLogOut={()=> setCurrentUSer(null)}/>}>
+            <Route path='/' element={<Home/>} />
+            <Route path="/login" element={<LogIn onLogIn={setCurrentUSer}/>}/>
+              
+            <Route path="/product" element={<AddProduct/>}/>
+          </Route>
+          
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
